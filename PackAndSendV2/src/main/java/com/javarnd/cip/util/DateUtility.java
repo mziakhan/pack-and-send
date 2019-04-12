c2fd36f8b405344ba74f0eb7b39878d72362c871
anasmohdkhan <49563598+anasmohdkhan@users.noreply.github.com>
2019-04-12 12:11:19 -0400

    Commit by Anas
    added a comment

--- PackAndSendV2/src/main/java/com/javarnd/cip/util/DateUtility.java
+++ PackAndSendV2/src/main/java/com/javarnd/cip/util/DateUtility.java
@@ -4,6 +4,8 @@ import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+//Ading comments...Anas 
+
 public class DateUtility {
 	public static Date stringToSQLDate(String date) {
 		SimpleDateFormat sdf1 = new SimpleDateFormat("MM-dd-yyyy");
